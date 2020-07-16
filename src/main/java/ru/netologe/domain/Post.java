c79adfe8c44e35587bbb97d6499394dda939fe1a
tatiana-polyakova <tanch88@mail.ru>
2020-07-16 22:05:34 +0300

    new v2

--- src/main/java/ru/netologe/domain/Post.java
+++ src/main/java/ru/netologe/domain/Post.java
@@ -25,8 +25,5 @@ public class Post {
     private int date;
     private int time;
     private String text;
-    private List<String> likesAuthorsIds;
-    private List<String> commentsIds;
-    private List<String> repostsAuthorsIds;
     private int vews;
 }
